c112947f343a97cda83e36ec4a70fd9477e77c83
nicolas.laurance@gmail.com <nicolas.laurance@gmail.com@b8d2c4c1-a5dd-0097-dd2a-bb409eaf283a>
2011-07-25 14:55:13 +0000

    mark version for 0.1.1

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.1.1'
 
 setup(name='elixirext.localized',
       version=version,
@@ -10,10 +10,10 @@ setup(name='elixirext.localized',
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
+      author='Nicolas Laurance, Yannick Brehon',
+      author_email='nicolas[dot]laurance<at>gmail[dot]com',
+      url='http://code.google.com/p/elixirlocalized',
+      license='GPL',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
